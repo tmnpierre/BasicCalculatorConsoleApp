cce7cca6b9edcb549f57262f34fd6f8f38320627
tmnpierre <tomyn.pierre@gmail.com>
2024-01-01 13:38:56 +0100

    feat: add basic arithmetic methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace BasicCalculatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        static double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        static double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                Console.WriteLine("Error: Division by zero.");
+                return 0;
+            }
+            else
+            {
+                return num1 / num2;
+            }
+        }
+    }
+}
